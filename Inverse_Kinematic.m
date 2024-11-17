5e3ded70f82ec073c3cc75cf38185b8062bb93b8
Dao Anh Phi <freeakaphi2k3@gmail.com>
2024-11-17 10:28:13 +0700

    17/11/2024

--- Inverse_Kinematic.m
+++ Inverse_Kinematic.m
@@ -12,14 +12,8 @@ function [theta1,theta2,theta3,valid]=Inverse_Kinematic(x3,y3,z3)
 
 
     %% calculate 
-    z3=z3-d1;
-    y3=y3=d1;
-    x3=x3-d1;
-
-    A0_3=[x3;y3;z3;1]*pinv([0;0;0;1])
-
-    c3=(x3^2+y3^2+z3^2-a2^2-a3^2)/(2*a2*a3);
-    if(sqrt(x3^2+y3^2+z3^2)>a2+a3||sqrt(x3^2+y3^2+z3^2)<abs(a2-a3))
+    z3p=z3-d1;
+    if(sqrt(x3^2+y3^2+z3p^2)>a2+a3||sqrt(x3^2+y3^2+z3p^2)<abs(a2-a3))
         theta1=0;
         theta2=0;
         theta3=0;
@@ -27,15 +21,15 @@ function [theta1,theta2,theta3,valid]=Inverse_Kinematic(x3,y3,z3)
         return
     else
     end
-
+    c3=(x3^2+y3^2+z3p^2-a2^2-a3^2)/(2*a2*a3);
     
+    s3=sqrt(1-c3^2);
 
-    theta3=acos(c3);
-    s3=sin(theta3);
+    theta3=atan2(s3,c3);
 
-    c2=(sqrt(x3^2+y3^2)*(a2+a3*c3)+z3*a3*s3)/(a2^2+a3^2+2*a2*a3*c3);
+    c2=(sqrt(x3^2+y3^2)*(a2+a3*c3)+z3p*a3*s3)/(a2^2+a3^2+2*a2*a3*c3);
 
-    s2=-(sqrt(x3^2+y3^2)*(a3*s3)+z3*(a2+a3*c3))/(a2^2+a3^2+2*a2*a3*c3);
+    s2=(-sqrt(x3^2+y3^2)*(a3*s3)+z3p*(a2+a3*c3))/(a2^2+a3^2+2*a2*a3*c3);
 
     theta2=atan2(s2,c2);
 
